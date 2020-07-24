a7f5c7dd309e82dc8951cbdf3ca888d95b73999c
tpgurdl92 <tpgurdl92@gmail.com>
2020-07-24 14:50:46 +0900

    add AD by Park

--- src/api/Room/createRoom/createRoom.js
+++ src/api/Room/createRoom/createRoom.js
@@ -1,26 +1,44 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { USER_FRAGMENT } from "../../../fragments";
+import { USER_FRAGMENT, ROOM_FRAGMENT } from "../../../fragments";
 export default {
   Mutation: {
-    createRoom: async (_, args) => {
-      const { userId } = args;
+    createRoom: async (_, args, request) => {
+      const { userid: userId } = request.request.headers;
+      console.log(userId);
+      const { data, location } = args;
+      console.log("createRoom");
       const date = new Date();
       const user = await prisma.user({ id: userId }).$fragment(USER_FRAGMENT);
       //이미 채팅방이 존재하는 유저의 id
-      let exceptionUserIdList = user.rooms.map((item) => {
-        if (item.participantA.id === userId) {
-          return item.participantB.id;
-        } else {
-          return item.participantA.id;
-        }
-      });
+      let exceptionUserIdList = [];
+      console.log(user);
+      if (user.rooms === null) {
+        exceptionUserIdList = user.rooms.map((item) => {
+          if (item.participantA.id === userId) {
+            return item.participantB.id;
+          } else {
+            return item.participantA.id;
+          }
+        });
+      }
       exceptionUserIdList.push(userId);
       //ban한 유저들의 아이디는 볼필요가 없네.
-      const userList = await prisma.users({
-        where: { id_not_in: exceptionUserIdList, location: user.location },
+      let userList = await prisma.users({
+        where: { id_not_in: exceptionUserIdList, location: location },
         first: 1,
       });
-      const participantB = userList[0];
+      let participantB = userList[0];
+      //보내고 싶은 지역의 유저를 찾지 못 했을 경우, 다른 지역의 유저도 검색한다.
+      if (participantB === null || participantB === undefined) {
+        userList = await prisma.users({
+          where: { id_not_in: exceptionUserIdList },
+          first: 1,
+        });
+        participantB = userList[0];
+        if (participantB === null || participantB === undefined) {
+          throw Error("can not find user to send message");
+        }
+      }
       console.log(participantB);
       //해당 유저의 비행기 개수가 1이상인지 확인
       if (user.availablePlane < 1) {
@@ -31,16 +49,29 @@ export default {
         data: { availablePlane: user.availablePlane - 1 },
         where: { id: userId },
       });
-      const room = await prisma.createRoom({
-        participantA: { connect: { id: userId } },
-        participantB: { connect: { id: participantB.id } },
-        lastCheckTimeA: date,
-        lastCheckTimeB: date,
-        isAlive: true,
-      });
+      const room = await prisma
+        .createRoom({
+          participantA: { connect: { id: userId } },
+          participantB: { connect: { id: participantB.id } },
+          lastCheckTimeA: date,
+          lastCheckTimeB: date,
+          isAlive: true,
+        })
+        .$fragment(ROOM_FRAGMENT);
 
       if (room) {
-        return room;
+        console.log(room);
+        const message = await prisma.createMessage({
+          type: "text",
+          data: data,
+          from: { connect: { id: userId } },
+          to: { connect: { id: participantB.id } },
+          room: { connect: { id: room.id } },
+        });
+        if (message) {
+          room.message = message;
+          return room;
+        }
       } else {
         throw error("Not found Rooms data");
       }
