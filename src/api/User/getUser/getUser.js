efdac71d95cc8d52629f82aaadb3773baa12d66d
lee <doguri1031@gmail.com>
2020-07-24 15:41:12 +0900

    editUser fix data type

--- src/api/User/getUser/getUser.js
+++ src/api/User/getUser/getUser.js
@@ -1,22 +1,22 @@
-import { prisma } from "../../../../generated/prisma-client";
+import {prisma} from "../../../../generated/prisma-client";
 
 export default {
-  Query: {
-    getUser: async (_, __, request) => {
-      const { userid: userId } = request.request.headers;
-      console.log("userid:" + userId);
-      try {
-        const user = await prisma.user({ id: userId });
-        if (user) {
-          console.log(user);
-          return user;
-        } else {
-          throw Error("there was no user with the Id");
+    Query: {
+        getUser: async (_, __, request) => {
+            const {userid: userId} = request.request.headers;
+            console.log("userid:" + userId);
+            try {
+                const user = await prisma.user({id: userId});
+                if (user) {
+                    console.log(user);
+                    return user;
+                } else {
+                    throw Error("there was no user with the Id");
+                }
+            } catch (e) {
+                console.log(e);
+                throw Error("failed to get User Information");
+            }
         }
-      } catch (e) {
-        console.log(e);
-        throw Error("failed to get User Information");
-      }
-    },
-  },
+    }
 };
--- src/api/User/login/login.js
+++ src/api/User/login/login.js
@@ -1,21 +1,22 @@
-import { prisma } from "../../../../generated/prisma-client";
+import { prisma } from '../../../../generated/prisma-client';
 
 export default {
-  Query: {
-    login: async (_, args) => {
-      console.log("im in1");
-      const { machineId } = args;
-      try {
-        const user = await prisma.user({ machineId });
-        if (user) {
-          return user;
-        } else {
-          return null;
-        }
-      } catch (e) {
-        console.log(e);
-        throw Error("failed to require machineId");
-      }
+    Query: {
+        login: async (_, args) => {
+            console.log('im in1');
+            const { machineId } = args;
+            try {
+                const user = await prisma.user({ machineId });
+                if (user) {
+                    console.log(user);
+                    return user;
+                } else {
+                    return null;
+                }
+            } catch (e) {
+                console.log(e);
+                throw Error('failed to require machineId');
+            }
+        },
     },
-  },
 };
