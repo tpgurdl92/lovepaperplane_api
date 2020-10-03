b17f963c53e2a82439a63d21d4a67dbf8a0465ee
tpgurdl92 <tpgurdl92@gmail.com>
2020-10-03 12:23:06 +0900

    readMessage

--- src/api/Message/readMessage/readMessage.js
+++ src/api/Message/readMessage/readMessage.js
@@ -1,19 +1,24 @@
 import { prisma } from "../../../../generated/prisma-client";
+import { MESSAGEREAD_FRAGMENT } from "../../../fragments";
 
 export default {
   Mutation: {
     readMessage: async (_, args, { request }) => {
       const date = new Date();
-      const { readeFlgId } = args;
-      const readFlg = await prisma.updateReadFlg({
-        data: { checkedTime: date },
-        where: { id: readeFlgId },
-      });
+      const { readFlgId } = args;
+      console.log(readFlgId);
+      const readFlg = await prisma
+        .updateReadFlg({
+          data: { checkedTime: date },
+          where: { id: readFlgId },
+        })
+        .$fragment(MESSAGEREAD_FRAGMENT);
 
       if (readFlg) {
-        return true;
+        console.log("return success");
+        return readFlg;
       } else {
-        return false;
+        throw ERROR("ddd");
       }
     },
   },
